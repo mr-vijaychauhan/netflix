90eda6c74c62ebc227055ff2fa9a7c32de8b8d88
VIJAY CHAUHAN <mr.vijaychauhan08@gmail.com>
2023-07-15 18:26:31 +0530

    changes done

--- src/app/Request.js
+++ src/app/Request.js
@@ -1,9 +1,4 @@
-// const key ='5f0b2e3e71a6b80fe6c2ceb1c8d9c5af'
-
 const key=process.env.TMDB_API_KEY;
-
-//https://api.themoviedb.org/3/movie/now_playing?api_key=5f0b2e3e71a6b80fe6c2ceb1c8d9c5af&language=en-US&page=1
-//https://api.themoviedb.org/3/search/movie?api_key=5f0b2e3e71a6b80fe6c2ceb1c8d9c5af&language=en-US&query=action&page=1&include_adult=false
 
 const requests = {
     requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
@@ -13,7 +8,8 @@ const requests = {
     requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
     requestNowPlaying: `https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1`,
     requestTvShows: `https://api.themoviedb.org/3/tv/popular?api_key=${key}&language=en-US&page=1`,
-    imagePath: 'https://image.tmdb.org/t/p/original'
+    imagePath: 'https://image.tmdb.org/t/p/original',
+    imagePath_w500: 'https://image.tmdb.org/t/p/w500'
   };
 
   export default requests
